bf03e8a640d67ae7685931b357d841836071add5
Kurt Krafft <kkrafft6609@gmail.com>
2020-06-18 13:47:12 -0500

    added ratings endpoint

--- hiredapp/views/employee_profiles/employee_profiles.py
+++ hiredapp/views/employee_profiles/employee_profiles.py
@@ -6,13 +6,14 @@ from rest_framework.response import Response
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework import status
-from hiredapp.models import Job, Customer, EmployeeProfile
+from hiredapp.models import Job, Customer, EmployeeProfile, Rating
 from django.db.models import Count, F, When, Case, IntegerField
 from django.utils import timezone
 from hiredapp.views.customers.customer import CustomerSerializer
 # from hiredapp.views.customers.user import UserSerializer
 import json 
-
+import sqlite3 
+from hiredapp.views.connection import Connection
 
 
 class EmployeeProfileSerializer(serializers.HyperlinkedModelSerializer):
@@ -24,14 +25,38 @@ class EmployeeProfileSerializer(serializers.HyperlinkedModelSerializer):
             view_name="employee_profiles",
             lookup_field = "id"
         )
-        fields = ('id', 'customer', 'title', 'description', 'job_type_id')
+        fields = ('id', 'customer', 'title', 'description', 'job_type_id', 'ratings')
         depth =2
+
 
 class EmployeeProfiles(ViewSet):
 
     def list(self, request):
 
         eps = EmployeeProfile.objects.all()
+        for prof in eps:
+            with sqlite3.connect(Connection.db_path) as conn:
+                conn.row_factory = create_ratings
+
+                db_cursor = conn.cursor()
+   
+                db_cursor.execute('''
+                SELECT 
+                    SUM(r.number) total,
+                    COUNT(r.employee_profile_id) as number,
+                    r.employee_profile_id
+                    from hiredapp_rating r
+                    where employee_profile_id = ?
+                ''',(prof.id,))
+         
+                data = db_cursor.fetchone()
+                if data.total is not None:
+                    avg = data.total/data.number
+                    prof.ratings = avg
+                
+            # print(data)
+            # avg = ratings['total']/ ratings['number']÷
+            # prof.ratings = ratings  
 
         #Check to see if the duery has a filter by user_id attached to it
         user_query = self.request.query_params.get('user_id', None)
@@ -42,6 +67,7 @@ class EmployeeProfiles(ViewSet):
             customer = Customer.objects.get(user_id=user)
             #then we filter the profiles based off of that customer
             eps = eps.filter(customer = customer)
+        
 
         serializer = EmployeeProfileSerializer(eps, many=True, context = {'request': request})
         # Now we check if there is a search requested
@@ -62,6 +88,27 @@ class EmployeeProfiles(ViewSet):
             if title is not None:
                 new_list = list(filter(lambda d: title in d['title'].lower(), new_list))
             #Now we return the a response of the filtered data from the serializer
+            for prof in new_list:
+                print('profile',prof)
+                with sqlite3.connect(Connection.db_path) as conn:
+                    conn.row_factory = create_ratings
+
+                    db_cursor = conn.cursor()
+    
+                    db_cursor.execute('''
+                    SELECT 
+                        SUM(r.number) total,
+                        COUNT(r.employee_profile_id) as number,
+                        r.employee_profile_id
+                        from hiredapp_rating r
+                        where employee_profile_id = ?
+                    ''',(prof['id'],))
+            
+                    data = db_cursor.fetchone()
+                    if data.total is not None:
+                        avg = data.total/data.number
+                        prof.ratings = avg
+
             return Response(new_list)
         else:
             return Response(serializer.data)
@@ -74,6 +121,25 @@ class EmployeeProfiles(ViewSet):
 
         try: 
             ep = EmployeeProfile.objects.get(pk=pk)
+            
+            with sqlite3.connect(Connection.db_path) as conn:
+                conn.row_factory = create_ratings
+
+                db_cursor = conn.cursor()
+   
+                db_cursor.execute('''
+                SELECT 
+                    SUM(r.number) total,
+                    COUNT(r.employee_profile_id) as number,
+                    r.employee_profile_id
+                    from hiredapp_rating r
+                    where employee_profile_id = ?
+                ''',(ep.id,))
+         
+                data = db_cursor.fetchone()
+                if data.total is not None:
+                    avg = data.total/data.number
+                    ep.ratings = avg
 
             serializer = EmployeeProfileSerializer(ep, many=False, context={'request': request})
             return Response(serializer.data)
@@ -121,3 +187,15 @@ class EmployeeProfiles(ViewSet):
 
         except Exception as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
+
+def create_ratings(cursor, row):
+    row = sqlite3.Row(cursor, row)
+    rating = Rating()
+    rating.number = row["number"]
+    rating.total = row["total"]
+    rating.employee_profile_id = row["employee_profile_id"]
+
+    return rating
